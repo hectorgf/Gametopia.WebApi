45fa0b92851cca0ec27aac08ae7985dc1beddb05
Hector <hector@peopleofthe.cloud>
2024-12-04 17:33:09 +0100

    First commit

---
+++ GametopiaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gametopia.WebAPI.Data
+{
+    public class GametopiaDbContext : DbContext
+    {
+        public GametopiaDbContext(DbContextOptions<GametopiaDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configuración de relaciones y restricciones
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Gametopia.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configurar DbContext
+builder.Services.AddDbContext<GametopiaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Configuración para la gestión del middleware de excepciones
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.Run();
